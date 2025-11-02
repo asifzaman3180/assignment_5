db712e24cd0fe5085aab693f75d770d23eb9e9b3
mazharulhaque947 <mazharulhaque94721@gmail.com>
2025-11-03 05:43:39 +0600

    task1

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class EmployeeManager {
     public static void main(String[] args) {
-        // Check arguments
+       
+
+	    // Check arguments
         if (args[0].equals("l")) {
             System.out.println("Loading data ...");
             try {
@@ -16,9 +18,14 @@ public class EmployeeManager {
                 for (String emp : e) {
                     System.out.println(emp);
                 }
-            } catch (Exception e) {}
+            }
+
+	    catch (Exception e) {}
             System.out.println("Data Loaded.");
-        } else if (args[0].equals("s")) {
+        } 
+
+
+	else if (args[0].equals("s")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
@@ -30,9 +37,14 @@ public class EmployeeManager {
                 Random rand = new Random();
                 int idx = rand.nextInt(e.length);
                 System.out.println(e[idx]);
-            } catch (Exception e) {}
+            }
+
+	    catch (Exception e) {}
             System.out.println("Data Loaded.");
-        } else if (args[0].contains("+")) {
+        } 
+
+
+	else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
                 BufferedWriter w = new BufferedWriter(
@@ -40,9 +52,13 @@ public class EmployeeManager {
                 String n = args[0].substring(1);
                 w.write(", " + n);
                 w.close();
-            } catch (Exception e) {}
+            } 
+
+	    catch (Exception e) {}
             System.out.println("Data Loaded.");
-        } else if (args[0].contains("?")) {
+        } 
+
+	else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
@@ -58,9 +74,13 @@ public class EmployeeManager {
                         found = true;
                     }
                 }
-            } catch (Exception e) {}
+            }
+
+	    catch (Exception e) {}
             System.out.println("Data Loaded.");
-        } else if (args[0].contains("c")) {
+        } 
+
+	else if (args[0].contains("c")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
@@ -81,9 +101,13 @@ public class EmployeeManager {
                     }
                 }
                 System.out.println(count + " word(s) found " + chars.length);
-            } catch (Exception e) {}
+            }
+
+	    catch (Exception e) {}
             System.out.println("Data Loaded.");
-        } else if (args[0].contains("u")) {
+        }
+
+	else if (args[0].contains("u")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
@@ -101,9 +125,13 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", e));
                 w.close();
-            } catch (Exception e) {}
+            } 
+
+	    catch (Exception e) {}
             System.out.println("Data Updated.");
-        } else if (args[0].contains("d")) {
+        } 
+
+	else if (args[0].contains("d")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
@@ -118,7 +146,9 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", list));
                 w.close();
-            } catch (Exception e) {}
+            }
+
+	    catch (Exception e) {}
             System.out.println("Data Deleted.");
         }
     }
