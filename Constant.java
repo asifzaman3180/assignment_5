b722bdb09fdca9f983f4fb0b9aeaabbf7e6e2026
Milon Hosen <mdmilon@gmail.com>
2025-11-03 01:50:04 +0600

    Task 5 : Replaced string literals with constant

---
+++ Constant.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static final String EMPLOYEE_FILE = "employees.txt";
+    public static final String DATA_LOADED = "Data Loaded.";
+    public static final String DATA_UPDATED = "Data Updated.";
+    public static final String DATA_DELETED = "Data Deleted.";
+    public static final String LOADING_DATA = "Loading data ...";
+    public static final String EMPLOYEE_FOUND = "Employee found!";
+}
