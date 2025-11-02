88ccc86b1361f99e7dbe552bf31e48db4e351c1d
Nubah <nubahmahjabin@gmail.com>
2025-11-03 00:08:38 +0600

    Replace String Literals with Constants

---
+++ Constants.java
@@ -0,0 +1,7 @@
+
+public class Constants {
+    public static final String EMPLOYEE_FILE = "employees.txt";
+    public static final String LOADING_MSG = "Loading data ...";
+    public static final String DATA_LOADED_MSG = "Data Loaded.";
+    
+}
